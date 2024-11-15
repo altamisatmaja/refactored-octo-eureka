ce201d43609430dfefa1d251be6a7c7b976a79c4
Altamis Fattah Atmaja <altamisfattah16@gmail.com>
2024-11-16 06:02:26 +0700

    fix: button responsive

--- resources/views/client/layouts/header.blade.php
+++ resources/views/client/layouts/header.blade.php
@@ -22,9 +22,11 @@
                                 <a href="/rekomendasi">Rekomendasi<span></span></a>
                             </li>
                         </ul>
-                        <div class="menu_side_area hidden md:block"> <!-- This will hide on mobile (default) and show on medium screens and above -->
+                        <div class="menu_side_area">
                             @auth
-                                <a href="{{ route('logout') }}" class="btn-main" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <a href="{{ route('logout') }}" class="btn-main btn-wallet" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    <!-- Show logout icon for logged-in users -->
+                                    <i class="fa fa-sign-out-alt"></i>
                                     <span>Logout</span>
                                 </a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -33,13 +35,16 @@
                             @endauth
 
                             @guest
-                                <a href="/masuk" class="btn-main">
+                                <a href="/masuk" class="btn-main btn-wallet">
+                                    <!-- Show login icon for guests -->
+                                    <i class="fa fa-sign-in-alt"></i>
                                     <span>Login</span>
                                 </a>
                             @endguest
 
                             <span id="menu-btn"></span>
                         </div>
+
 
                     </div>
                 </div>
